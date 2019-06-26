732663f22fb2d43cad8455ac55c333fc931d9c7b
Richard Parada <richardparada@Richards-MacBook-Pro.local>
2019-06-26 17:12:04 -0400

    cleaning up folders

--- Capstone Project/eda.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-df = pd.read_csv('/Users/richardparada/Desktop/Data_Science/Capstone Project/NSDUH_2016_Tab.csv')
-print(df.describe())
-
-df.plot(data=df['CIGEVER'], kind='box')
-plt.show()
--- Capstone Project/response_codes.py
+++
@@ -1,8 +0,0 @@
-#survey response codes. found on page i-23 of codebook
-
-never_used_drug_codes = [91, 991, 9991]
-used_drug_codes = [93, 993, 9993]
-dont_know_codes = [94, 994, 9994]
-refused_codes = [97, 997, 9997]
-blank_codes = [98, 998, 9998] #now answered. not asked
-legitmate_skip_codes = [99, 999, 9999]
